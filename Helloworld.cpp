e928934bf0aacc5a9f9a19156e2ce4a2db2b5491
Juls Akhaika <julianaakhaika@gmail.com>
2023-09-27 19:32:41 +0300

    fresh

--- Helloworld.cpp
+++ Helloworld.cpp
@@ -1,8 +1,22 @@
-#include <iostream> 
+// Hello world program in C++
+/*This C++ file was created by 
+juliana 169930
+*/
+
+#include <iostream> //preprocessor directive
 using namespace std;
 
- int main()
-  {
-    cout<< "Hello World. This is my first C++ program";
+  int main()
+//   {
+//     cout<< "Hello World!!";
      
- }
+//  }
+
+{
+  int number;
+  cout<< " Enter a number: ";
+  cin >> number;
+  cout<< number + " is the number you entered " << endl;
+  cerr<< " This is an example of an error message";
+  
+}
