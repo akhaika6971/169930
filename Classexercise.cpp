bb60e732808b0a2c91d15505ac6c7feb85cf8386
Juls Akhaika <julianaakhaika@gmail.com>
2023-09-28 10:53:23 +0300

    class exercise

--- Classexercise.cpp
+++ Classexercise.cpp
@@ -5,4 +5,4 @@ int main()
     cout<< "Welcome to the wonderful world of C++!" << endl;
     cerr<< "This is my first C++ output statement ""\n" ;
     cout<< "Soon i will learn how to input data into my program";
-}
+} 
