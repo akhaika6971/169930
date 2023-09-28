f10324aeb653a391d401de08ef747a3025d56578
Juls Akhaika <julianaakhaika@gmail.com>
2023-09-28 10:42:29 +0300

    changes

---
+++ Classexercise.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<< "Welcome to the wonderful world of C++!" << endl;
+    cerr<< "This is my first C++ output statement ""\n" ;
+    cout<< "Soon i will learn how to input data into my program";
+}
