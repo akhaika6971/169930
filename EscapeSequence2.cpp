3e7915480e1c028f1530ff70171de10a3dfc8781
Juls Akhaika <julianaakhaika@gmail.com>
2023-10-04 18:24:29 +0300

    class work

---
+++ EscapeSequence2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"Strathmore University Vision\n";
+    cout<<".............................\n";
+    cout<<"To become a leading out-come driven entrepreneurial\n";
+    cout<<"research University by translating our excellence into\n";
+    cout<<"major contribution to culture, economic well-being and\n";
+    cout<<"quality of life";
+}
