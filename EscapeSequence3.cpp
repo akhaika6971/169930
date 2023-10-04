fd4dbd3cac53efc43a11957b5ed0cfad48c02312
Juls Akhaika <julianaakhaika@gmail.com>
2023-10-04 18:45:15 +0300

    class work

---
+++ EscapeSequence3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout<<"*****\"Escape Sequence Practice\"*****\n";
+    cout<<"DBIT is a university diploma designed to focus on:\n";
+    cout<<"  1.Development\n";
+    cout<<"  2.Implementation, and\n";
+    cout<<"  3.Management of information systems.";
+
+}
